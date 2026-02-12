c4284b053ca2a2eaf2b051b5076dda1b46c0365b
Matiyas Dawit <matid@uw.edu>
2026-02-12 14:54:27 -0800

    Day 1: Added image_info, load_image, to_grayscale, save_image, display_image and resize_image to basic_image_ops.py

---
+++ src/basic_image_ops.py
@@ -0,0 +1,64 @@
+import cv2 as cv
+
+
+def image_info(img):
+  '''
+  This function takes an image array and return basic information
+  like the min/max pix values, dtype and shape
+
+  Args: img is the image array 
+
+  '''
+  pass
+
+def load_image(img_path):
+  '''
+  This function takes an file path and will display it in the 
+  NumPy array 
+
+  Args: img is the image array
+  '''
+  pass
+
+def to_grayscale(img):
+  '''
+  This function takes an image array and changes it to the 
+  graysscale
+  
+  Args: img is the image array
+  '''
+  pass
+
+
+def save_image(img, img_path):
+  '''
+  This function will save the image array the file path to 
+  where it will be saved  
+  This will help in debugging and reproducing results
+
+
+  Args: img is the image array 
+        img_path is the path 
+  '''
+  pass 
+
+def display_image(img):
+  '''
+  This function will display the image array to a screen 
+  usign Google colab friendly libraries 
+
+  Args: img is the image array 
+  '''
+  pass
+
+def resize_image(img, img_width, img_height):
+  '''
+  This function will take the image array and resize it to
+  the new dimensions
+
+  Args: img is the iamge array 
+        img_width is the new image width
+        img_height is the new image height
+  '''
+  pass 
+
